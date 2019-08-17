12e98030180ae0f0cfec81a59c066f261d587709
Peter de Jersey <peter.dejersey@me.com>
2019-08-17 20:04:33 +1000

    Testing session 17082019

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,35 +1,42 @@
 #define OUTPUT_PIN 9
-#define MODE_BUTTON 8
 #define ENABLED HIGH
 
 //TENS Modes
 #define HAMMERING   0
 #define ACUPUNCTURE 1
-#define NARAPATHY   2
+#define NAPRAPATHY  2
 #define CUPPING     3
 #define SCRAPING    4
 #define MASSAGE     5
 
+//TENS BUTTONS
+#define MODE_BUTTON     8
+#define HZ_BUTTON       7
+#define BUTTON_DELAY 120 
+
+#define DEBUG 1
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
   pinMode(12,OUTPUT); 
   pinMode(11,OUTPUT); 
   pinMode(10,OUTPUT); 
   pinMode(OUTPUT_PIN,OUTPUT); 
   pinMode(MODE_BUTTON,OUTPUT); 
+  pinMode(HZ_BUTTON,OUTPUT); 
 
   digitalWrite(12, HIGH); 
   digitalWrite(11, HIGH); 
   digitalWrite(10, HIGH); 
   digitalWrite(OUTPUT_PIN, HIGH); 
   digitalWrite(MODE_BUTTON, HIGH); 
+  digitalWrite(HZ_BUTTON, HIGH); 
 
-  off(); 
-  switchMode(SCRAPING); 
-  delay(2000);
-  switchMode(ACUPUNCTURE); 
-  delay(2000);
-  switchMode(MASSAGE); 
+  off();
+   
+  delay(1500);
+  deepMode(LOW); 
 }
 
 void switchMode(int mode) {
@@ -43,14 +50,27 @@ void switchMode(int mode) {
   else if (mode < current_mode) {
     delta = mode + (6-current_mode); 
   }
-
+  
   for (int i = 0; i < delta; i++) {
     digitalWrite(MODE_BUTTON, LOW); 
-    delay(125); 
+    delay(BUTTON_DELAY); 
     digitalWrite(MODE_BUTTON, HIGH); 
-    delay(125); 
+    delay(BUTTON_DELAY); 
   } 
   current_mode = mode;
+}
+
+void deepMode(int enabled) {
+
+  static int current_state = LOW; 
+  
+  if (enabled != current_state) {
+    digitalWrite(HZ_BUTTON, LOW); 
+    delay(BUTTON_DELAY); 
+    digitalWrite(HZ_BUTTON, HIGH); 
+    delay(BUTTON_DELAY); 
+    current_state = enabled; 
+  }
 }
 
 void lowPower(void) {
@@ -73,20 +93,212 @@ void highPower(void) {
   digitalWrite(OUTPUT_PIN, LOW); 
 }
 
-void off(void) {
-  digitalWrite(12, HIGH); 
-  digitalWrite(11, HIGH); 
-  digitalWrite(10, HIGH); 
+void off(void) { 
+
+  if (DEBUG) {
+    Serial.println("Off"); 
+  }
   digitalWrite(OUTPUT_PIN, HIGH); 
+  deepMode(LOW); 
+}
+void on(void) {
+  digitalWrite(OUTPUT_PIN, LOW); 
+}
+
+void effectTingling(int iterations) {
+  off(); 
+  switchMode(ACUPUNCTURE); 
+  delay(1750);  
+
+  for (int i = 0; i < iterations; i++) {
+    on();
+    delay(1000); 
+    off(); 
+    delay(2000); 
+  }
+}
+
+//========================================
+//          effectStab
+//  Low power hammering for 3s.
+//  High power stab for 500ms
+//
+//  Total: 3.5s
+//========================================
+void effectStab(void) {
+
+  if (DEBUG) {
+    Serial.println("Stabbing"); 
+  }
+  
+  off(); 
+  switchMode(HAMMERING); 
+  lowPower(); 
+  delay(3000); 
+  highPower();
+  delay(750); 
+  lowPower(); 
+}
+
+void effectDeepStab(void) {
+
+  if (DEBUG) {
+    Serial.println("Deep stabbing"); 
+  }
+  off(); 
+  deepMode(HIGH); 
+  switchMode(HAMMERING); 
+  highPower(); 
+  delay(3750); 
+}
+
+
+//========================================
+//          effectTearing
+//========================================
+void effectTearing(void) {
+
+  if (DEBUG) {
+    Serial.println("Tearing"); 
+  }
+  
+  off(); 
+  switchMode(NAPRAPATHY); 
+
+  highPower(); 
+  delay(10000); 
+
+  deepMode(HIGH); 
+  delay(7000); 
+
+  mediumPower();
+  delay(5000); 
+
+  highPower();
+  deepMode(HIGH); 
+  delay(10000); 
+}
+
+//========================================
+//          effectPeriodPain
+//   High power accupuncture
+//========================================
+void effectPeriodPain(void) {
+
+  if (DEBUG) {
+    Serial.println("Period pain"); 
+  }
+
+  off(); 
+  switchMode(ACUPUNCTURE); 
+  highPower(); 
+  delay(10000); 
+  
+  off(); 
+  mediumPower(); 
+  delay(5000); 
+  
+  highPower(); 
+  delay(2500); 
+
+  deepMode(HIGH); 
+  delay(3000); 
+  deepMode(LOW); 
+}
+  
+
+//========================================
+//          effectBluntStab
+//========================================
+void effectBluntStab(void) {
+
+  if (DEBUG) {
+    Serial.println("Blunt stab"); 
+  }
+  
+  off(); 
+  switchMode(ACUPUNCTURE); 
+  
+  lowPower(); 
+  delay(1500); 
+  
+  mediumPower(); 
+  deepMode(HIGH); 
+  delay(1000); 
+  
+  lowPower(); 
+  deepMode(LOW);  
+}
+
+void effectBackground(void) {
+
+  off(); 
+  switchMode(NAPRAPATHY); 
+  deepMode(HIGH); 
+  mediumPower();  
+}
+
+void effectDeepScraping(void) {
+
+  if (DEBUG) {
+    Serial.println("Deep scrape"); 
+  }
+  
+  off(); 
+  switchMode(SCRAPING); 
+  deepMode(HIGH); 
+  highPower(); 
+
+  delay(10000); 
+  off(); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
- 
-//  lowPower(); 
+//
+  //effectDeepStab(); 
+//  effectTearing(); 
+//  
+
+    //effectTearing(); 
+
+  //effectPeriodPain();
+//
+//  effectBackground();  
+//  while(1);
+//
+//  effectStab(); 
+//  off(); 
 //  delay(2000); 
-//  mediumPower(); 
-//  delay(2000); 
+//  effectDeepStab(); 
+  //effectDeepScraping(); 
+//  off(); 
 //  highPower(); 
-//  delay(2000);`
+
+  
+//  while(1);
+  while(Serial.available() == 0); 
+  Serial.println("Start"); 
+  
+  // Stab 3 times - 10.5 seconds
+  for (int i = 0; i < 3; i++) {
+    effectStab(); 
+  }
+
+  //varying degrees of period pain for 41 seconds
+  effectPeriodPain(); 
+  effectPeriodPain(); 
+
+  //Tearing for 20.5 seconds
+  effectTearing(); 
+
+  //Deep stab for 3.5 seconds (one stab) 
+  effectDeepStab(); 
+
+  //Tearing for 20.5 seconds
+  effectTearing(); 
+ 
+  //Total 10.5 + 41 + 20.5 + 3.5 + 20.5 = 96 seconds (1.5 min)
+  off(); 
+  Serial.println("Done"); 
+  while(1); 
 }
